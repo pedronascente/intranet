a226dcea4babb0b8e6e70c1eb70f86edf9765bb7
Pedro Nascente <nascente3d@hotmail.com>
2023-11-23 12:23:12 -0300

    foi configurado a classe comissaoController pra listar todas as comissões de acordo com a planilha informada por parametro na função getComissoes

--- app/Http/Controllers/Comissao/ComissaoController.php
+++ app/Http/Controllers/Comissao/ComissaoController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Comissao;
 
 use App\Models\Comissao\Meio;
 use App\Models\Comissao\Planilha;
-use App\Models\Comissao\ServicoAlarme;
-use App\Models\Comissao\TecnicaDeRastreamento;
-use App\Models\comissao\EntregaDeAlarme;
-use App\Models\comissao\ComercialRastreamentoVeicular;
-use App\Models\comissao\ComercialAlarmeCercaEletricaCFTV;
 use App\Http\Controllers\Controller;
+use App\Models\Comissao\ServicoAlarme;
+use App\Models\comissao\EntregaDeAlarme;
+use App\Models\comissao\PortariaVirtual;
+use App\Models\Comissao\ReclamacaoDeCliente;
+use App\Models\Comissao\TecnicaDeRastreamento;
+use App\Models\comissao\ComercialRastreamentoVeicular;
+use App\Models\comissao\SupervisaoComercialRastreamento;
+use App\Models\comissao\TecnicaAlarmesCercaEletricaCFTV;
+use App\Models\comissao\ComercialAlarmeCercaEletricaCFTV;
+use App\Models\comissao\SupervisaoComercialAlarmesCercaEletricaCFTV;
+use App\Models\comissao\SupervisaoTecnicaESacAlarmesCercaEletricaCFTV;
 
 class ComissaoController extends Controller
 {
@@ -17,6 +23,10 @@ class ComissaoController extends Controller
     {
         $planilha = Planilha::with('colaborador', 'periodo', 'tipoPlanilha')->findOrFail($id);
         $comissoes = $this->getComissoes($planilha->tipoPlanilha->formulario, $id);
+
+
+        // dd($id, $comissoes, $planilha);
+
         $data = [
             'titulo' => $planilha->tipoPlanilha->nome,
             'planilha' => $planilha,
@@ -33,6 +43,7 @@ class ComissaoController extends Controller
         if ($this->getMeio($planilha->tipoPlanilha->formulario)) {
             $data['meios'] = Meio::all();
         }
+
         return view('comissao.index', $data);
     }
 
@@ -68,6 +79,7 @@ class ComissaoController extends Controller
     {
         $dataArrayFormularioAlarmes = [
             'comercialAlarmeCercaEletricaCFTV',
+            'portariaVirtual',
         ];
         if (in_array($tipoPlanilha, $dataArrayFormularioAlarmes)) {
             return true;
@@ -77,19 +89,27 @@ class ComissaoController extends Controller
 
     private function getComissoes($tipoPlanilha)
     {
-
         switch ($tipoPlanilha) {
             case 'tecnicaDeRastreamento':
                 return TecnicaDeRastreamento::orderBy('id', 'desc')->paginate(10);
             case 'comercialAlarmeCercaEletricaCFTV':
-
-                return ComercialAlarmeCercaEletricaCFTV::with(['servico', 'meio'])
-                    ->orderBy('id', 'desc')->paginate(10);
+                return ComercialAlarmeCercaEletricaCFTV::orderBy('id', 'desc')->paginate(10);
             case 'comercialRastreamentoVeicular':
-
                 return ComercialRastreamentoVeicular::orderBy('id', 'desc')->paginate(10);
             case 'entregaDeAlarmes':
                 return EntregaDeAlarme::orderBy('id', 'desc')->paginate(10);
+            case 'portariaVirtual':
+                return PortariaVirtual::orderBy('id', 'desc')->paginate(10);
+            case 'reclamacaoDeCliente':
+                return ReclamacaoDeCliente::orderBy('id', 'desc')->paginate(10);
+            case 'supervisaoComercialAlarmesCercaEletricaCFTV':
+                return SupervisaoComercialAlarmesCercaEletricaCFTV::orderBy('id', 'desc')->paginate(10);
+            case 'supervisaoComercialRastreamento':
+                return SupervisaoComercialRastreamento::orderBy('id', 'desc')->paginate(10);
+            case 'supervisaoTecnicaESACAlarmesCercaEletricaCFTV':
+                return SupervisaoTecnicaESacAlarmesCercaEletricaCFTV::orderBy('id', 'desc')->paginate(10);
+            case 'tecnicaAlarmesCercaEletricaCFTV':
+                return TecnicaAlarmesCercaEletricaCFTV::orderBy('id', 'desc')->paginate(10);
             default:
                 // Lidar com outros casos ou lançar uma exceção, se necessário
                 return null;
