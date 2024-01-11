460fc4313e4adc7f26e49e8b7e5f92d480b192d6
Pedro Nascente <nascente3d@hotmail.com>
2024-01-11 17:17:35 -0300

    foi criado o componnent btn-excluir

---
+++ resources/views/components/botao/btn-excluir.blade.php
@@ -0,0 +1,6 @@
+<a href="javascript:void(0)" class="btn btn-danger btn-sm"
+    data-toggle="modal" data-target="#deleteModal"
+    data-route="{{ $rota }}"
+    title="{{ $titulo }}">
+    <i class="fas fa-trash"></i> Excluir
+</a>
